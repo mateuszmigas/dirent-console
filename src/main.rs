e7c204e2760fdf32faa6da0d4592320d6dd8a774
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2024-12-07 13:32:36 +0100

    different approach

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,64 @@
-use std::io;
+fn main() {
+    let context = Context::new(1, "dark");
+    let root = app(&context);
+    println!("{:?}", root);
+}
 
-mod components;
-mod macros;
-mod tui;
+#[derive(Debug)]
+struct Node {
+    key: String,
+    children: Vec<Node>,
+}
 
-fn main() -> io::Result<()> {
-    let mut terminal = ratatui::init();
-    let tui_result: Result<(), io::Error> = crate::tui::Tui::default().run(&mut terminal);
-    ratatui::restore();
-    tui_result
+struct Context {
+    user_id: u32,
+    theme: String,
+}
+
+impl Context {
+    fn new(user_id: u32, theme: &str) -> Self {
+        Self {
+            user_id,
+            theme: theme.to_string(),
+        }
+    }
+
+    fn render(&self, component: impl Fn(&Self) -> Node) -> Node {
+        component(self)
+    }
+}
+
+impl Node {
+    fn new(key: &str) -> Self {
+        Self {
+            key: key.to_string(),
+            children: vec![],
+        }
+    }
+
+    fn add_child(mut self, child: Node) -> Self {
+        self.children.push(child);
+        self
+    }
+}
+
+fn button(context: &Context, key: &str, text: &str) -> Node {
+    Node::new(key)
+}
+
+fn panel(context: &Context, key: &str, title: &str) -> Node {
+    let button1 = context.render(|_| button(context, "button1", title));
+
+    Node::new(key)
+        .add_child(button(context, "button1", title))
+        .add_child(button(context, "button2", "Input"))
+}
+
+fn app(context: &Context) -> Node {
+    let left_panel = panel(context, "panel1", "Left");
+    let right_panel = panel(context, "panel2", "Right");
+
+    Node::new("app")
+        .add_child(left_panel)
+        .add_child(right_panel)
 }
